4bca4d5f9c2d61454e02c89888f639eb83f6cd2b
Adrienne Grieco <adrienne.grieco@gmail.com>
2017-10-01 12:15:34 -0700

    parsing client argument

---
+++ WarmupAssignment/fingerclient.cpp
@@ -0,0 +1,110 @@
+/* fingerclient using TCP connection to connect to server
+ * IP Address: cs1.seattleu.edu, Port Number: 10042 */
+ // 5th on roster: using Port#s 10040-10049
+// TODO: test on cs2.seattleu.edu
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <iostream>
+#include <limits>
+
+#define PORT_NUMBER "10042"
+#define MAXDATASIZE 100
+
+/* Checks for valid argument format (username@hostname:server_port): so the '@'
+ * and ':' symbols are present in the expected order and each item (username,
+ * hostname,and server_port) are at least 1 character in length */
+bool arg_format_is_valid(std::size_t at_pos, std::size_t colon_pos,
+  std::string user_arg) {
+    return (at_pos != std::string::npos && colon_pos != std::string::npos &&
+        at_pos > 0 && colon_pos > (at_pos + 1) &&
+        user_arg.length() > (colon_pos + 1));
+}
+
+int main(int argc, char *argv[]) {
+
+  if(argc < 2) {
+    fprintf(stderr, "usage %s username@hostname:server_port\n", argv[0]);
+    exit(0);
+  }
+  // parse user argument
+  std::string user_arg = std::string(argv[1]);
+  std::string username, hostname, server_port;
+  printf("user_arg: %s\n", user_arg.c_str());
+  std::size_t at_pos, colon_pos;
+  at_pos = user_arg.find("@");
+  colon_pos = user_arg.find(":");
+  printf("at_pos: %d, colon_pos: %d\n", at_pos, colon_pos);
+
+  if(arg_format_is_valid(at_pos, colon_pos, user_arg)) {
+    username = user_arg.substr(0, at_pos);
+    hostname = user_arg.substr(at_pos + 1, colon_pos - (at_pos + 1));
+    server_port = user_arg.substr(colon_pos + 1);
+  }
+  else {
+    fprintf(stderr, "usage %s username@hostname:server_port\n", argv[0]);
+    exit(0);
+  }
+  printf("username: %s\nhostname: %s\nserver_port: %s\n",
+    username.c_str(), hostname.c_str(), server_port.c_str());
+
+  int sockfd;
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+
+  int numbytes;
+  char buf[100];
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;  // use AF_INET6 to force IPv6
+  hints.ai_socktype = SOCK_STREAM;
+
+  // if((rv = getaddrinfo("cs1.seattleu.edu", PORT_NUMBER, &hints, &servinfo)) != 0) {
+  if((rv = getaddrinfo(hostname.c_str(), server_port.c_str(), &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    exit(1);
+  }
+
+  // loop through all the results and connect to the first one possible
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      perror("socket");
+      continue;
+    }
+    if(connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      perror("connect");
+      close(sockfd);
+      continue;
+    }
+    break;  // if code reaches this point, connection was made successfully
+  }
+
+  freeaddrinfo(servinfo); // free memory
+
+  // if p reached NULL with no connection, print error and exit
+  if(p == NULL) {
+    fprintf(stderr, "failed to connect\n");
+    exit(2);
+  }
+
+  if((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+    perror("recv");
+    exit(1);
+  }
+
+  buf[numbytes] = '\0';
+
+  printf("Message from server: '%s'\n", buf);
+
+  // close the connection with the server
+  close(sockfd);
+
+  return 0;
+}
---
+++ WarmupAssignment/fingerserver.cpp
@@ -0,0 +1,116 @@
+/* fingerserver using TCP connection for multiple client connections
+* IP Address: cs1.seattleu.edu, Port Number: 10042 */
+// 5th on roster: using Port# 10040-10049
+// TODO: test on cs2.seattleu.edu
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <iostream>
+#include <unistd.h>
+
+// const int CONNECTIONS_ALLOWED = 5;  // usually 20
+// const char* PORT_NUMBER = "10042";
+
+#define PORT_NUMBER "10042"
+#define CONNECTIONS_ALLOWED 10
+
+int main(int argc, char * argv[]) {
+
+  int sockfd, new_sockfd;
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage client_addr;
+  socklen_t addr_size;
+  int rv;
+  pid_t cp_id;  // child process id
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;  // use AF_INET6 to force IPv6
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;  // uses current IP address
+                                // (clients expect cs1.seattleu.edu)
+
+  if((rv = getaddrinfo(NULL, PORT_NUMBER, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    exit(1);
+  }
+
+  // loop through all the results and connect to the first one possible
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      perror("socket");
+      continue;
+    }
+    if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      perror("bind");
+      close(sockfd);
+      continue;
+    }
+    break;  // if code reaches this point, connection was made successfully
+  }
+
+  freeaddrinfo(servinfo); // free memory
+
+  // if p reached NULL with no bind, print error and exit
+  if(p == NULL) {
+    fprintf(stderr, "failed to bind socket\n");
+    exit(1);
+  }
+
+  if(listen(sockfd, CONNECTIONS_ALLOWED) == -1) {
+    perror("listen");
+    exit(1);
+  };
+
+  // accept incomming connections
+  while(true) {
+    addr_size = sizeof client_addr;
+    new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_size);
+    if(new_sockfd == -1) {
+      perror("accept");
+      continue;
+    }
+
+    // TODO: replace with method to retrieve user addr as username@
+    // inet_ntop(client_addr.ss_family,
+    //   get_in_addr((struct sockaddr *)&client_addr), s, sizeof s);
+
+    cp_id = fork();
+    if(cp_id == -1) {
+      perror("fork");
+      exit(1);
+    }
+
+    if(cp_id == 0) {  // child process executing
+      close(sockfd);  // child shouldn't have a listener
+
+      if(send(new_sockfd, "Hello, world!", 13, 0) == -1) {
+        perror("send");
+      }
+      // if((dup2(new_sockfd, 1))!= 1 || (dup2(new_sockfd, 2)) != 2) {
+      //   perror("dup2");
+      // }
+      // dup2(new_sockfd, 1);
+      // dup2(new_sockfd, 2);
+
+      if((execl("/usr/bin/finger", "finger", "griecoa1", NULL)) == -1) {
+        perror("execl");
+      };
+
+      close(new_sockfd);
+      exit(0);
+    }
+    else {  // parent process executing
+      close(new_sockfd);  // parent shouldn't keep child sockets
+    }
+  }
+
+
+  return 0;
+}
